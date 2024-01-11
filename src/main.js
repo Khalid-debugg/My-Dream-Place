39999f3b202a1bcd810fccba584107b5b3d1cef6
Khalid-debugg <khalidsherif.dev@gmail.com>
2024-01-11 18:04:31 +0200

    Add pagination library and custom styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import dataPersistence from "./plugins/dataPersistencePlugin";
+import VueAwesomePaginate from "vue-awesome-paginate";
+import "vue-awesome-paginate/dist/style.css";
 import "./input.css";
 
 const pinia = createPinia();
@@ -10,6 +12,7 @@ pinia.use(dataPersistence);
 const app = createApp(App);
 
 app.use(pinia);
+app.use(VueAwesomePaginate);
 app.use(router);
 
 app.mount("#app");
--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -226,6 +226,13 @@
           :hotelID="hotel.property.id"
           :hotelDescription="hotel.accessibilityLabel"
         />
+        <!-- <vue-awesome-paginate
+          :total-items="50"
+          :items-per-page="5"
+          :max-pages-shown="5"
+          v-model="currentPage"
+          :on-click="onClickHandler"
+        /> -->
       </main>
     </div>
   </div>
@@ -324,4 +331,30 @@ export default {
   components: { SearchBar, Switch, HotelCard },
 };
 </script>
-<style lang="scss" scoped></style>
+<style>
+.pagination-container {
+  display: flex;
+  justify-content: center;
+  column-gap: 10px;
+}
+.paginate-buttons {
+  height: 40px;
+  width: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: rgb(242, 242, 242);
+  border: 1px solid rgb(217, 217, 217);
+  color: black;
+}
+.paginate-buttons:hover {
+  background-color: #d8d8d8;
+}
+.active-page {
+  background-color: #3498db;
+  border: 1px solid #3498db;
+  color: white;
+}
+.active-page:hover {
+  background-color: #2988c8;
+}
+</style>
